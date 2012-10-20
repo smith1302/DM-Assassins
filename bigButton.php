1b2fc3d2cd56f13e709171c3ce69be48e5e48e94
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 17:05:54 -0400

    resource issue

--- bigButton.php
+++ bigButton.php
@@ -10,13 +10,13 @@ $myTeam = mysql_result($sql0,0,'team');
 echo mysql_result($sql0,0,'name');
 
 $sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin where assassin.team=target.team AND team != $myTeam ORDER BY rand() limit 1");
+$sql1 = mysql_fetch_row($sql1);
 
 if (!mysql_num_rows($sql1))
 {
 	$sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin ORDER BY rand() limit 1");
-
+	$sql1 = mysql_fetch_row($sql1);
 }
-$sql1 = mysql_fetch_row($sql1);
 print_r($sql1);
 echo mysql_error();
 
