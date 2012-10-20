7a82bf9873b3c746eb05f89b6205f7d6d581f33f
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 17:54:26 -0400

    see if data is getting passed

--- getUser.php
+++ getUser.php
@@ -4,7 +4,7 @@ require_once("connectToServer.php");
 connect();
 
 $userPin = $_POST['pin'];
-
+echo $userPin;
 $sql0 = mysql_query("Select * FROM users where pin = $userPin");
 $user = mysql_fetch_assoc($sql0);
 
