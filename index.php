f163f175b7d5235a0ca03a2d7704d4beed616c71
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 16:30:13 -0400

    center

--- index.php
+++ index.php
@@ -127,7 +127,7 @@ The Assassins Staff";
 		
 		
 		$result = mysql_query("SELECT * FROM $table where team=$team AND usertype = 1");
-		echo('<a id="bigbutton" href="#" class="round">Big Red<br />Button</a>');
+		echo('<a id="bigbutton" href="#" class="round">Big Red<br />Button</a><br />');
 				echo("If your target's Facebook is inaccessible you can email<br /> their overall at: <a href='mailto:$overallEmail'>$overallEmail</a>");
 	}
 	else if ($alive)
