e48d0a81b2333b137906f44494a5ce521c54587c
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 17:12:22 -0400

    fixed

--- bigButton.php
+++ bigButton.php
@@ -9,9 +9,9 @@ $sql0 = mysql_query("Select * from users where pin = $pin");
 $myTeam = mysql_result($sql0,0,'team');
 echo mysql_result($sql0,0,'name');
 
-$sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin where assassin.team=target.team AND team != $myTeam ORDER BY rand() limit 1");
+$sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin where assassin.team=target.team AND assassin.team != $myTeam ORDER BY rand() limit 1");
 
-if (false) //(mysql_num_rows($sql1) == 0)
+if (mysql_num_rows($sql1) == 0)
 {
 	$sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin ORDER BY rand() limit 1");
 }
