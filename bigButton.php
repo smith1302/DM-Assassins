e4b96d11e6f204a9c03818869f3f25ad86887e73
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 17:04:24 -0400

    deal with 0 row situation

--- bigButton.php
+++ bigButton.php
@@ -6,9 +6,16 @@ connect();
 $pin = $_POST['pin'];
 
 $sql0 = mysql_query("Select * from users where pin = $pin");
+$myTeam = mysql_result($sql0,0,'team,');
 echo mysql_result($sql0,0,'name');
 
-$sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin where assassin.team=target.team ORDER BY rand() limit 1");
+$sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin where assassin.team=target.team AND team != $myTeam ORDER BY rand() limit 1");
+
+if (!mysql_num_rows($sql1))
+{
+	$sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin ORDER BY rand() limit 1");
+
+}
 $sql1 = mysql_fetch_row($sql1);
 print_r($sql1);
 echo mysql_error();
