322326eb8711380313b6470db7d0ec9d3204fc82
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 17:18:07 -0400

    lookup user pair

--- bigButton.php
+++ bigButton.php
@@ -15,7 +15,17 @@ if (mysql_num_rows($sql1) == 0)
 {
 	$sql1 = mysql_query("Select assassin.pin, assassin.team, target.pin, target.team from users target join users as assassin on assassin.target = target.pin ORDER BY rand() limit 1");
 }
-	$sql1 = mysql_fetch_row($sql1);
+
+$sql1 = mysql_fetch_row($sql1);
+
+$assassinPin = mysql_query("SELECT pin FROM users where target = $pin");
+$assassinPin = mysql_result($assassin,0,"pin");
+$targetPin =  mysql_query("SELECT target FROM users where pin = $pin");
+$targetPin = mysql_result($assassin,0,"target");
+
+echo ("assassinPin: $assassinPin<br />");
+echo ("targetPin: $targetPin<br />");
+
 print_r($sql1);
 echo mysql_error();
 
