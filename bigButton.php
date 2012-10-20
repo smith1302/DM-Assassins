28a8e55de12070c49e6c5c17149bcdfc30407c6c
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 16:51:21 -0400

    proper function

--- bigButton.php
+++ bigButton.php
@@ -9,7 +9,7 @@ $sql0 = mysql_query("Select * from users where pin = $pin");
 echo mysql_result($sql0,0,'name');
 
 $sql1 = mysql_query("Select assassin.pin, assassin.team, target.team, target.name from users target join users as assassin on assassin.target = target.pin where assassin.pin=target.pin ORDER BY rand() limit 1");
-$sql1 = mysql_result($sql1,0);
+$sql1 = mysql_fetch_row($sql1);
 print_r($sql1);
 echo mysql_error();
 
