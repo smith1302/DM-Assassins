e88470b61a608bb8dbc0dbf76c3fec5f0daed16a
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 21:20:44 -0400

    added twitter for new target

--- index.php
+++ index.php
@@ -173,7 +173,12 @@ $('#bigbutton').click(function(){
 			$('#info').fadeOut(0);
 			console.log(newUser);
 			newUser = $.parseJSON(newUser);
-			$('#info').html('<p>Name: '+newUser['name'] + '<br />Facebook: <a href="http://facebook.com/' + newUser['facebook'] +'">http://facebook.com/'+ newUser['facebook'] + '</a> <br />Email: <a href="mailto:'+ newUser['email'] + '">'+ newUser['email'] + '</a><br />Team: '+newUser['team']+'</p><br /><img src ="uploads/'+newUser['img']+'" class="overall_img" />');
+			var twitterInfo = '';
+			if (newUser['twitter'])
+			{
+				twitterInfo = '<br /><a href="http://twitter.com/'+ newUser['twitter'] + '">http://twitter.com/'+newUser['twitter']+'</a>';
+			}
+			$('#info').html('<p>Name: '+newUser['name'] + '<br />Facebook: <a href="http://facebook.com/' + newUser['facebook'] +'">http://facebook.com/'+ newUser['facebook'] + '</a>'+ twitterInfo + ' <br />Email: <a href="mailto:'+ newUser['email'] + '">'+ newUser['email'] + '</a><br />Team: '+newUser['team']+'</p><br /><img src ="uploads/'+newUser['img']+'" class="overall_img" />');
 			$('#info').fadeIn(1000);
 		});
 	});	
