ef7bb8747c9e2c0a48375a01ea36bf15ea30a666
Matthew Gerstman <mattgerstman@gmail.com>
2012-10-20 21:21:40 -0400

    fixed twitter formatting

--- index.php
+++ index.php
@@ -176,7 +176,7 @@ $('#bigbutton').click(function(){
 			var twitterInfo = '';
 			if (newUser['twitter'])
 			{
-				twitterInfo = '<br /><a href="http://twitter.com/'+ newUser['twitter'] + '">http://twitter.com/'+newUser['twitter']+'</a>';
+				twitterInfo = '<br />Twitter: <a href="http://twitter.com/'+ newUser['twitter'] + '">http://twitter.com/'+newUser['twitter']+'</a>';
 			}
 			$('#info').html('<p>Name: '+newUser['name'] + '<br />Facebook: <a href="http://facebook.com/' + newUser['facebook'] +'">http://facebook.com/'+ newUser['facebook'] + '</a>'+ twitterInfo + ' <br />Email: <a href="mailto:'+ newUser['email'] + '">'+ newUser['email'] + '</a><br />Team: '+newUser['team']+'</p><br /><img src ="uploads/'+newUser['img']+'" class="overall_img" />');
 			$('#info').fadeIn(1000);
